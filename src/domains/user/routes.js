bdab2c376f34a7e92b27de1c9369d6375b14f57d
Israel E. Filgueira <israel.eriston@gmail.com>
2018-05-06 20:04:53 -0500

    refactoring architeture the schemes

--- src/domains/user/routes.js
+++ src/domains/user/routes.js
@@ -2,14 +2,10 @@
 
 const middleware = require('./middlewares')
 const service = require('./service')
-const schema = require('./schemes')
-
-console.log('schemes', schema)
 
 const routes = [
   { method: 'get',
     path: '/user',
-    schema: schema.create,
     handler: middleware.find,
     service: [ service.find, service.getOne ]
   }
--- src/domains/user/schemes.js
+++ src/domains/user/schemes.js
@@ -1,14 +1,11 @@
 const Joi = require('joi')
 
-const validate = require('../../helpers/schema')
-
 const create = Joi.object().keys({
   username: Joi.string().alphanum().min(3).max(30).required(),
   password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
   access_token: [Joi.string(), Joi.number()]
 }).with('username').without('password', 'access_token')
-const schemes = ({
-  create: () => ({ body: validate(create) })
-})
-console.log('schemes.js', schemes)
-module.exports = schemes
+
+module.exports = {
+  create
+}
