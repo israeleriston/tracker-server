4760f773ca6864181293ba9905d0e12775720882
Fábio Santos <8303937+fabiofdsantos@users.noreply.github.com>
2018-07-27 15:57:27 +0100

    Fix typo

--- src/server/plugins/routes.js
+++ src/server/plugins/routes.js
@@ -3,10 +3,10 @@
 const path = require('path')
 const registerRoutes = require('fastify-register-routes')
 
-const defaultPaht = path.join(__dirname, '../../domains')
+const defaultPath = path.join(__dirname, '../../domains')
 
 const install = server => {
-  server.register(registerRoutes, { path: defaultPaht, useService: false })
+  server.register(registerRoutes, { path: defaultPath, useService: false })
 }
 
 module.exports = { install }
